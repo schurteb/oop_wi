2660cb77438e4b2094b7d1450090ab4745efe9cf
marcoglaus-teko <marco.glaus@edu.teko.ch>
2021-10-31 12:49:14 +0100

    init

---
+++ src/ch/teko/oop/Main.java
@@ -0,0 +1,8 @@
+package ch.teko.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Willkommen im Fach Objektorientiertes Programmieren!");
+    }
+}
---
+++ src/ch/teko/oop/tag01/input/Main.java
@@ -0,0 +1,7 @@
+package ch.teko.oop.tag01.input;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
