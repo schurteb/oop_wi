2e2658b895e9ce37ebf5f22d28e7b7f05bce4938
marcoglaus-teko <marco.glaus@edu.teko.ch>
2021-11-17 09:45:05 +0100

    signature method example

---
+++ src/ch/teko/oop/tag03/input/SignaturMethode.java
@@ -0,0 +1,23 @@
+package ch.teko.oop.tag03.input;
+
+public class SignaturMethode {
+
+    public int calc(int i) {
+        return i * i;
+    }
+
+    /*
+    public double calc(int i) {
+        return i*i;
+    }
+    */
+
+    public int calc(short s) {
+        return s * s;
+    }
+
+    public int calc(int i, int ii) {
+        return i * ii;
+    }
+
+}
