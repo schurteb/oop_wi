fc7a5573c5be0f24a0cbbde4cca541abf8998505
marcoglaus-teko <marco.glaus@edu.teko.ch>
2021-12-01 06:09:20 +0100

    Input Tag 5

---
+++ src/ch/teko/oop/tag05/input/Main.java
@@ -0,0 +1,48 @@
+package ch.teko.oop.tag05.input;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        System.out.println(arr1[3]);
+
+        int[] arr2 = {1,2,3,4,5};
+        System.out.println(arr2[3]);
+
+        String[] arr3 = new String[3];
+        System.out.println(arr3[2]);
+
+        String[] arr4 = {"Das", "ist", "ein", "Array"};
+        System.out.println(arr4[2]);
+
+        double[] arr5 = {1.1,2.2,3.3,4.4,5.5};
+        System.out.println(arr5);
+
+        for (int i = 0; i <= arr5.length; i++){
+            System.out.println(arr5[i]);
+        }
+
+        String[] arr6 = new String[4];
+
+        arr6[0] = "TEST";
+        arr6[1] = "TEST";
+
+        for (short i=2; i < arr6.length; i++){
+            arr6[i] = "test";
+        }
+
+        for (int i=0; i < arr6.length; i++){
+            System.out.println(arr6[i]);
+        }
+
+        int[] arr7 = new int[5];
+        System.out.println(arr7[arr7.length-1]);
+        Arrays.fill(arr7, 4);
+        System.out.println(arr7[arr7.length-1]);
+        arr7[arr7.length-1] = 2;
+        System.out.println(arr7[arr7.length-1]);
+        Arrays.sort(arr7);
+        System.out.println(arr7[arr7.length-1]);
+    }
+}
